7c6d49f7fc4b6a0576ec7bbfd22ef540b1964d0b
mrmusuadhi <m.r.musuadhi.rajan@student.rug.nl>
2021-06-23 13:36:38 +0200

    4Players_4Dice

--- src/components/GameVisualization.vue
+++ src/components/GameVisualization.vue
@@ -1,27 +1,64 @@
 <template>
+
     <v-card outlined class="game-vis">
-        <v-card-title>
-            Game visualisation
-        </v-card-title>
-        <v-card-text v-if="round">
-            <div v-for="(player, i) in round.players">
-                Player {{player}} rolled {{round.dice[i]}}
-            </div>
-            Common knowledge {{ round.commonKnowledge }}
-            <br>
-            Winner this round: {{ winner }}
-            <div v-if="simulationResults">
-                Latest simulation results: {{ simulationResults }}
-            </div>
-        </v-card-text>
+      <v-card-title>
+          Game visualisation
+      </v-card-title>
+        
+      <v-container>   
+        <div v-if="round.players.length <= 2">
+          <v-row>
+            <v-col v-for="(player, i) in round.players" :key="'player-'+i">
+              <player-item :index="i" :round="round" :player="player"/>            
+            </v-col> 
+          </v-row> 
+        </div>
+        <div v-if="round.players.length === 3">
+          <v-row>
+            <v-col>
+              <player-item :index="1" :round="round" :player="round.players[1]"/>            
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <player-item :index="0" :round="round" :player="round.players[0]"/>            
+            </v-col>
+            <v-col>
+              <player-item :index="2" :round="round" :player="round.players[2]"/>            
+            </v-col>
+          </v-row>                  
+        </div>   
+        <div v-if="round.players.length === 4">
+          <v-row>
+            <v-col>
+              <player-item :index="1" :round="round" :player="round.players[1]"/>            
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <player-item :index="0" :round="round" :player="round.players[0]"/>            
+            </v-col>
+            <v-col>
+              <player-item :index="2" :round="round" :player="round.players[2]"/>            
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <player-item :index="3" :round="round" :player="round.players[3]"/>            
+            </v-col>
+          </v-row>          
+        </div>             
+      </v-container>
     </v-card>
 </template>
 
 <script>
 import {mapGetters, mapState} from "vuex";
+import PlayerItem from './PlayerItem.vue';
 
 export default {
     name: "GameVisualization",
+    components: {PlayerItem},
     computed: {
         ...mapGetters(['round']),
         ...mapState({
@@ -29,6 +66,14 @@ export default {
             winner: state => state.game.winner,
         }),
     },
+    methods: {
+      getPlayerImgUrl(playerId) {        
+        return '../assets/player' + playerId + ".png"
+      },
+      getDiceImgUrl(diceId) {
+        return '../assets/dice_' + diceId + ".png"
+      }
+    }
 }
 </script>
 
---
+++ src/components/PlayerItem.vue
@@ -0,0 +1,42 @@
+<template>    
+    <div class="player-item">        
+        <div class="align-center">Player {{player + 1}}</div>
+        <v-col align="center">
+            <v-avatar color="green" size="75">
+            <img :src="getPlayerImgUrl(index + 1)" :alt="'Player' + (index+1)" />
+            </v-avatar>
+        </v-col>
+        <v-col align="center">
+            <v-row>
+            <v-col v-for="(dice, i) in round.dice[index]" :key="'dice-' + i" align="center">
+                <v-avatar size="42">
+                <img :src="getDiceImgUrl(dice)" :alt="'Dice' + (index+1)" />
+                </v-avatar>
+            </v-col>
+            </v-row>
+        </v-col>
+    </div>
+</template>
+<script>
+export default {
+    name: "PlayerItem",  
+    props: ["index", "round", "player"], 
+    methods: {
+      getPlayerImgUrl(playerId) {        
+        return '../assets/player' + playerId + ".png"
+      },
+      getDiceImgUrl(diceId) {
+        return '../assets/dice_' + diceId + ".png"
+      }
+    }
+}
+</script>
+<style scoped>
+.player-item {
+    width: 150px;
+    margin: auto;
+}
+.align-center {
+  text-align: center;
+}
+</style>
